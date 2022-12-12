becd56da81f2a0f33fa134e5185872255eb2ebd3
smkelly1691 <skelly1691@gmail.com>
2022-12-11 22:09:53 -0800

    removes unused imports

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,4 @@
-import { useContext, useState } from 'react'
-import Brightness2Icon from '@material-ui/icons/Brightness2'
-import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded'
+import { useState } from 'react'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
 import { projects, skills, contact } from '../../portfolio'
